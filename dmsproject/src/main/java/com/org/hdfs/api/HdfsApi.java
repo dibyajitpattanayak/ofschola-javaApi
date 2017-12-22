5c55e00d60d66bdfe47b95ed0e989d05cdfe06de
Dibyajita <dibyajitpattanayak@gmail.com>
2017-12-23 01:47:07 +0530

    designed

--- dmsproject/src/main/java/com/org/hdfs/api/HdfsApi.java
+++ dmsproject/src/main/java/com/org/hdfs/api/HdfsApi.java
@@ -1,12 +1,12 @@
 package com.org.hdfs.api;
 
 public class HdfsApi {
-public static void main(String[] args) {
-	System.out.println("This is HdfsApi class");
-	System.out.println("1st commit made");
-for(int i=0;i<=10;i++)
-{
-	System.out.println(i);
-}
-}
+	public static void main(String[] args) {
+		System.out.println("This is HdfsApi class");
+		System.out.println("1st commit made");
+		for(int i=0;i<=10;i++)
+		{
+			System.out.println(i);
+		}
+	}
 }
