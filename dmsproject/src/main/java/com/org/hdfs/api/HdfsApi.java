960a406a47211eebd7a6c393ed6f212a2472ebef
Dibyajita Pattanayak <dibyajitpattanayak@gmail.com>
2017-12-22 19:56:05 +0530

    1st changes done

--- dmsproject/src/main/java/com/org/hdfs/api/HdfsApi.java
+++ dmsproject/src/main/java/com/org/hdfs/api/HdfsApi.java
@@ -3,5 +3,6 @@ package com.org.hdfs.api;
 public class HdfsApi {
 public static void main(String[] args) {
 	System.out.println("This is HdfsApi class");
+	System.out.println("1st commit made");
 }
 }
