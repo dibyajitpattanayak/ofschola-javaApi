a26c53e733a99c917601ae75bb5c531759b08f9f
Dibyajita <dibyajitpattanayak@gmail.com>
2017-12-23 01:59:23 +0530

    Added

--- dmsproject/src/main/java/com/org/hdfs/api/HdfsApi.java
+++ dmsproject/src/main/java/com/org/hdfs/api/HdfsApi.java
@@ -3,9 +3,5 @@ package com.org.hdfs.api;
 public class HdfsApi {
 public static void main(String[] args) {
 	System.out.println("This is HdfsApi class");
-	System.out.println("1st commit made");
-	System.out.println("Added one line from project");
-	System.out.println("te");
-	System.out.println("one more added");
 }
 }
