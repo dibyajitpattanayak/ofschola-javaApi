afd964d5268dd41e7c9a4d924e91fe7658ced4b3
Dibyajita <dibyajitpattanayak@gmail.com>
2017-12-23 20:15:34 +0530

    committed

--- dmsproject/src/main/java/com/org/hdfs/api/HdfsApi.java
+++ dmsproject/src/main/java/com/org/hdfs/api/HdfsApi.java
@@ -3,7 +3,7 @@ package com.org.hdfs.api;
 public class HdfsApi {
 public static void main(String[] args) {
 	System.out.println("This is HdfsApi class");
-	System.out.println("This is local branch 1");
+	System.out.println("This is jan branch ");
 	//new branch
 }
 }
