7fa4187fab42e26875080278ccad6922cab4c890
Dibyajita <dibyajitpattanayak@gmail.com>
2017-12-23 21:07:14 +0530

    made branch

--- dmsproject/src/main/java/com/org/hdfs/api/HdfsApi.java
+++ dmsproject/src/main/java/com/org/hdfs/api/HdfsApi.java
@@ -3,6 +3,5 @@ package com.org.hdfs.api;
 public class HdfsApi {
 public static void main(String[] args) {
 	System.out.println("this is master branch");
-	System.out.println("changes made for master branch");
 }
 }
