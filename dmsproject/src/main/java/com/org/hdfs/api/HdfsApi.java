e8efe918c57312f24d99effcb47bd2ca0c11ad72
Dibyajita <dibyajitpattanayak@gmail.com>
2017-12-23 20:45:34 +0530

    committed

--- dmsproject/src/main/java/com/org/hdfs/api/HdfsApi.java
+++ dmsproject/src/main/java/com/org/hdfs/api/HdfsApi.java
@@ -2,10 +2,6 @@ package com.org.hdfs.api;
 
 public class HdfsApi {
 public static void main(String[] args) {
-<<<<<<< HEAD
-	System.out.println("This is local branch");
-=======
 	System.out.println("This is Dec branch");
->>>>>>> branch 'localbranch1-1.0-Dec-Remote' of https://github.com/dibyajitpattanayak/ofschola-javaApi.git
 }
 }
