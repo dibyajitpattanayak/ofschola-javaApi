6876f48ddec86b2afd6376fa58deaf2046994dbb
Dibyajita <dibyajitpattanayak@gmail.com>
2017-12-23 20:23:05 +0530

    changes done

--- dmsproject/src/main/java/com/org/hdfs/api/HdfsApi.java
+++ dmsproject/src/main/java/com/org/hdfs/api/HdfsApi.java
@@ -2,8 +2,6 @@ package com.org.hdfs.api;
 
 public class HdfsApi {
 public static void main(String[] args) {
-	System.out.println("This is HdfsApi class");
 	System.out.println("This is jan branch ");
-	//new branch
 }
 }
