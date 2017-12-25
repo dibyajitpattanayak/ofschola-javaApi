7be81d72b53a9290874ff64c0bb2f441dc746209
Dibyajita <dibyajitpattanayak@gmail.com>
2017-12-25 20:26:11 +0530

    n

--- dmsproject/src/main/java/com/org/hdfs/api/HdfsApi.java
+++ dmsproject/src/main/java/com/org/hdfs/api/HdfsApi.java
@@ -1,7 +1,7 @@
 package com.org.hdfs.api;
 
 public class HdfsApi {
-public static void main(String[] args) {
-	System.out.println("This is jan branch ");
-}
-}
+	public static void main(String[] args) {
+		System.out.println("This is Dec branch");
+		System.out.println("resolved the conflict");
+}}
