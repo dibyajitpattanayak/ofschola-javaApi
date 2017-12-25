fc0565ef89fe6204b2e2235ab07c9ed16f41dc81
Dibyajita <dibyajitpattanayak@gmail.com>
2017-12-25 20:26:49 +0530

    m

--- dmsproject/src/main/java/com/org/hdfs/api/HdfsApi.java
+++ dmsproject/src/main/java/com/org/hdfs/api/HdfsApi.java
@@ -3,11 +3,4 @@ package com.org.hdfs.api;
 public class HdfsApi {
 	public static void main(String[] args) {
 		System.out.println("This is Dec branch");
-<<<<<<< HEAD
-		System.out.println("resolved the conflict");
 }}
-=======
-		System.out.println("resolved the conflict");
-		//
-	}}
->>>>>>> branch 'localbranch1-1.0-Dec-Remote' of https://github.com/dibyajitpattanayak/ofschola-javaApi.git
