b8fc290ea914f72ec8886cfe95d2ec31211983b4
Dibyajita <dibyajitpattanayak@gmail.com>
2017-12-30 16:42:32 +0530

    oupstfdnvvnuv

--- dmsproject/src/main/java/com/org/hdfs/api/HdfsApi.java
+++ dmsproject/src/main/java/com/org/hdfs/api/HdfsApi.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 	System.out.println("this is master branch");
 	System.out.println("indexed added");
 	System.out.println("new feture added");
+	System.out.println("hello");
 }
 }
